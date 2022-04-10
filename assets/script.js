9c590e3f48f048052f2414025b975f6a7237ed18
Snehita Kolli <snehitakolli20@gmail.com>
2022-04-09 22:32:23 -0700

    pseudocode done in js file

---
+++ assets/script.js
@@ -0,0 +1,10 @@
+// Dashboard with form inputs
+    // Can enter CITIES to get the weather 
+
+// WEATHER FORECAST
+    // CURRENT WEATHER: city name, date, icon of weather conditions, temperature, humidity, wind speed, UV index 
+        // UV index: color-coded for favorable, moderate, severe 
+    // FUTURE WEATHER: 5-day forecast--> date, icon of weather, temperature, humidity
+
+// Click on city from search history--> can see the search history again
+    // Use localStorage
